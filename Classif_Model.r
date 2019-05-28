731edebd1a53e1c16482558cdfb20e919462e0b1
Ian <ianduran60@gmail.com>
2019-05-28 17:23:20 -0600

    Added median input data

--- Classif_Model.r
+++ Classif_Model.r
@@ -1,14 +1,17 @@
 install.packages("randomForest")
 library(randomForest)
+library(dplyr)
 
 completeData <- read.csv("nci60_binary_class_training_set.csv", header = TRUE, sep = ",")
 
+completeData <- select(completeData, -LC.NCI.H23)
+View(completeData)
 ###
 Labels <- completeData$Labels
 
 ensembl_gene_id = completeData$ensembl_gene_id
 
-data2 <- completeData[,6:65]
+data2 <- completeData[,6:64]
 
 data <- log2(data2+0.001)
 
@@ -24,16 +27,21 @@ ommitedMissingData <- na.omit(data)
 # View(ommitedMissingData)
 
 # NAs filled with random value from observed data
-randomMissingData 
+randomMissingData  <- data
 #View(randomMissingData)
-  
+
+
 # NAs filled with medians
 medianMissingData <- na.roughfix(data)
 View(meanMissingData)
 
 # NAs filled with means
-meansMissingData
+meansMissingData <- data
 
-# NAs filled with quadra-thicc terms
-#quadraticMissingData <- mice(data, m = 1, maxit = 0, method = 'quadratic', seed = 1)
-#View(quadraticMissingData)
+for(i in 1:ncol(meansMissingData)){
+  meansMissingData[is.na(meansMissingData[,i]), i] <- mean(meansMissingData[,i], na.rm = TRUE)
+}
+
+View(meansMissingData)
+
+# NAs
