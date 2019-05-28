7dd7e2070909d314410abcd8ec99d2c4d0da72ee
Ian <ianduran60@gmail.com>
2019-05-28 15:05:55 -0600

    Changed script tp use na.omit and na.roughfix

--- Classif_Model.r
+++ Classif_Model.r
@@ -1,22 +1,34 @@
-install.packages("mice")
+install.packages("randomForest")
+library(randomForest)
 library(mice)
 
-data <- read.csv("nci60_binary_class_training_set.csv", header = TRUE, sep = ",")
-data$Labels <- as.factor(data$Labels)
-is.na(data)
+completeData <- read.csv("nci60_binary_class_training_set.csv", header = TRUE, sep = ",")
 
-ommitedMissingData <- data
+Labels <- completeData$Labels
+ensembl_gene_id = completeData$ensembl_gene_id
 
+data2 <- completeData[,6:65]
+
+data <- log2(data2+0.001)
+
+data <- cbind(data, Labels)
+
+data <- cbind(data, ensembl_gene_id)
+
+View(data)
+
+ommitedMissingData <- na.omit(data)
+View(ommitedMissingData)
 # NAs filled with random value from observed data
-randomMissingData <- mice(data, m = 1, maxit = 50, method = 'sample', seed = 500)
+randomMissingData <- mice(data, m = 1, maxit = 0, method = 'sample', seed = 500)
 View(randomMissingData)
   
-# NAs filled with means
-meanMissingData <- mice(data, m = 1, maxit = 50, method = 'mean', seed = 500)
+# NAs filled with medians
+meanMissingData <- na.roughfix(data)
 View(meanMissingData)
 
 # NAs filled with quadra-ticc terms
-quadraticMissingData <- mice(data, m = 1, maxit = 50, method = 'quadratic', seed = 500)
+quadraticMissingData <- mice(data, m = 1, maxit = 0, method = 'quadratic', seed = 1)
 View(quadraticMissingData)
   
 
