5493283fb4bf8860f81ee2752d1887c7fac25a03
Ian <ianduran60@gmail.com>
2019-05-28 16:47:59 -0600

    Cambios

--- Classif_Model.r
+++ Classif_Model.r
@@ -1,10 +1,11 @@
 install.packages("randomForest")
 library(randomForest)
-library(mice)
 
 completeData <- read.csv("nci60_binary_class_training_set.csv", header = TRUE, sep = ",")
 
+###
 Labels <- completeData$Labels
+
 ensembl_gene_id = completeData$ensembl_gene_id
 
 data2 <- completeData[,6:65]
@@ -14,22 +15,25 @@ data <- log2(data2+0.001)
 data <- cbind(data, Labels)
 
 data <- cbind(data, ensembl_gene_id)
+###
 
 View(data)
 
+# NAs ommited
 ommitedMissingData <- na.omit(data)
-View(ommitedMissingData)
+# View(ommitedMissingData)
+
 # NAs filled with random value from observed data
-randomMissingData <- mice(data, m = 1, maxit = 0, method = 'sample', seed = 500)
-View(randomMissingData)
+randomMissingData 
+#View(randomMissingData)
   
 # NAs filled with medians
-meanMissingData <- na.roughfix(data)
+medianMissingData <- na.roughfix(data)
 View(meanMissingData)
 
-# NAs filled with quadra-ticc terms
-quadraticMissingData <- mice(data, m = 1, maxit = 0, method = 'quadratic', seed = 1)
-View(quadraticMissingData)
-  
+# NAs filled with means
+meansMissingData
 
-
+# NAs filled with quadra-thicc terms
+#quadraticMissingData <- mice(data, m = 1, maxit = 0, method = 'quadratic', seed = 1)
+#View(quadraticMissingData)
